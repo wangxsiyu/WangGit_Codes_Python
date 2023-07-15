3a352774760e10170218efbab98a52a1cb6caa5a
Siyu Wang <wangxsiyu@gmail.com>
2023-07-14 23:49:14 -0400

    u

--- W__Env/W_Env/task_Tokens.py
+++ W__Env/W_Env/task_Tokens.py
@@ -1,42 +1,36 @@
 from gym import spaces
-from W_Gym.W_Gym import W_Gym
 from W_Gym.W_Gym_Grid2D import W_Gym_grid2D
-from W_Python import W_tools as W
+from W_Python.W import W
 import numpy as np
 import random
 
-class task_tokens(W_Gym_grid2D):
-    p_reward = None
-    def __init__(self, p_reward = 0.75, **kwarg):
+class task_Tokens(W_Gym_grid2D):
+    _param_task = {'p_reward': 1, 'CueValues': np.array([1,2,-1,-2]), 'endowment': 0, \
+                   }
+    def __init__(self, **kwarg):
         super().__init__(1,3,5, is_ITI = False, n_maxTrialsPerBlock = 180, **kwarg)
-        self.p_reward = p_reward
         # set action space
         self.action_space = spaces.Discrete(3) # stay, left, right
         # set rendering dimension names
-        self.setup_obs_Name2DimNumber({'fixation':0, 'image1':1, 'image2':2, 'image3':3, 'image4':4})
+        self.setup_obs_channel_namedict({'fixation':0, 'image1':1, 'image2':2, 'image3':3, 'image4':4})
         # set stages
-        stage_names = ["fixation", \
+        state_names = ["fixation", \
                        "choice"]
-        stage_advanceuponaction = ["choice"]
-        self.setW_stage(stage_names = stage_names, stage_advanceuponaction = stage_advanceuponaction)
+        state_immediateadvance = ["choice"]
+        self.setup_state_parameters(state_names = state_names, state_immediateadvance = state_immediateadvance,\
+                        state_timelimits = "ones")        
+        self.setup_human_keys_auto('binary_plus')
 
-    def _reset_block(self):
-        self.image_R = [-1,-2,1,2]
-        random.shuffle(self.image_R)
-    def _block_info(self):
-        self.info_block['image_R'] = self.image_R
-    
-    def _trial_info(self):
-        self.info_trial['params'] = self.param_trial 
-    
-    def _reset_trial(self):
+    def custom_reset_block(self):
+        self._param_block['ImageValues'] = random.shuffle(self._param_task['CueValues'])
+
+    def custom_reset_trial(self):
         self.gaze.set_pos(0,1)
         IM_id = np.random.choice(4,2, replace = False)
         R_side = [self.image_R[x] for x in IM_id]
         randv = 0+(np.random.rand(2) < self.p_reward)
         R_side = R_side * randv
-        param = {'IM_id': IM_id, 'R_side': R_side}
-        self.param_trial = param
+        self._param_trial = {'IM_id': IM_id, 'R_side': R_side}
 
     def _step_set_validactions(self):
         if self.metadata_stage['stage_names'][self.stage] in ["fixation"]:
